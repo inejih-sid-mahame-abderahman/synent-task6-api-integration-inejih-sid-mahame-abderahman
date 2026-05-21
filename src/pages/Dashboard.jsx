46ae7e2289add1de03a35da08375dc1a5f5b0419
inejih sid mhamed abderahman <inejihsidmhamed801@gmail.com>
2026-05-22 00:47:20 +0100

    start refractoring the ode alreadyexsisted in phases start phase 4

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white p-6">
+      <h1 className="text-4xl font-bold text-center">
+        Inejih GitHub Dashboard
+      </h1>
+
+      <p className="text-center text-slate-400 mt-4">
+        Personal GitHub API Integration Project
+      </p>
+    </div>
+  );
+};
+
+export default Dashboard;
