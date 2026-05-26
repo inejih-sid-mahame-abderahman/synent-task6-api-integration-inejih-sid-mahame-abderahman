34d25331aa07c4c034804ac50bd46a6dde0e723d
inejih sid mhamed abderahman <inejihsidmhamed801@gmail.com>
2026-05-26 14:52:59 +0100

    start refractoring the ode already exsisted in phases start phase 5

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ const Dashboard = () => {
   return (
     <div className="min-h-screen bg-slate-950 text-white p-6">
       <h1 className="text-4xl font-bold text-center">
-        Inejih GitHub Dashboard
+        Inejih sid mhamed GitHub Dashboard
       </h1>
 
       <p className="text-center text-slate-400 mt-4">
